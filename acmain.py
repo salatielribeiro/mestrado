6e3e44a40993e8be959cec50dc6dd31d1809ac0a
salatielribeiro <salatieltr@gmail.com>
2017-05-16 16:49:15 -0300

    16/05/17 16:48

--- acmain.py
+++ acmain.py
@@ -3,7 +3,7 @@
 from acarquivo import *
 
 def main():
-	caminho_arquivo_waze 	= '/home/salatiel/Desktop/dados/belo_horizonte/acidentes/waze/acidentes_belo_horizonte_waze_0616_0517.csv'
+	caminho_arquivo_waze 	= '/home/salatiel/Desktop/dados/belo_horizonte/acidentes/waze/eventos_belo_horizonte_waze_0616_0517.csv'
 	caminho_arquivo_bhtrans = '/home/salatiel/Desktop/dados/belo_horizonte/acidentes/bh_trans/'
 	formata_arquivo_acidente(arquivo=caminho_arquivo_waze,col_id=2,col_data_hora=24,cabecalho=False,tipo_arquivo='waze',apagar=False)
 	#tempo_medio_registro_starttime_waze(arquivo_=caminho_arquivo_waze,col_pubmillis=27, col_startime=4)
