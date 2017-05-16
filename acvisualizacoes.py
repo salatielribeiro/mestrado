e3784129e9e2395fd29438c0d8b6e42e964c8572
salatielribeiro <salatieltr@gmail.com>
2017-05-16 16:56:48 -0300

    16/05/17 16:48

---
+++ acvisualizacoes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def precipitacoes():
+
+def acidente_dia():
+  
+def acidente_dia_hora():
+
+def acidente_dia_semana():
+
+def acidente_mes():
+
+def acidente_evento():
